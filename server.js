85a43fc2ba16496c7099a34c4ddebdd0a6576300
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-24 22:24:58 -0400

    debugged and finalized

--- server.js
+++ server.js
@@ -2,31 +2,45 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const budget = require('./models/budget.js');
+let style = ""
 
 // express.static middleware
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false}))
 
 // index route
 app.get('/budgets/', (req, res) => {
+    let bankAccount = 0
     // res.send('hello - this is the index route')
+    budget.forEach((item) => { bankAccount += item.amount })
+            if(bankAccount < 0) {
+                style = "lowBalance"
+            } else if(bankAccount > 1000) {
+                style  = "highBalance"
+            }
     res.render('index.ejs', {
         allBudgets: budget,
+        bankAccount: bankAccount,
         title: 'index',
+        style: style,
     })
 })
 
+
+
 // new route
-app.post('/budgets/new', (req, res) => {
+app.get('/budgets/new', (req, res) => {
     // console.log(req.body)
     res.render('new.ejs')
 })
 
 // create route
 app.post('/budgets', (req, res) => {
-    budget.create(req.body, (error, createdBudget) => {
-        res.redirect("/budgets");
+    req.body.amount = parseInt(req.body.amount)
+    budget.push(req.body)  
+    res.redirect("/budgets");
     })
-})
+
 
 
 // show route
